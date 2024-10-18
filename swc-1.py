8317ef96301dacee924268db4d6f56a68f46bea2
Rebekah <rebekah.newman3@nhs.net>
2024-10-18 11:52:08 +0100

    created jupyter notebook

---
+++ swc-1.py
@@ -0,0 +1,7 @@
+weight_kg = 60.1
+patient_id = "001"
+weight_lb = 2.2 * weight_kg
+patient_id = "inflam_" + patient_id
+print(weight_lb)
+print(patient_id)
+print(patient_id, "weight in kilograms:", weight_kg)
